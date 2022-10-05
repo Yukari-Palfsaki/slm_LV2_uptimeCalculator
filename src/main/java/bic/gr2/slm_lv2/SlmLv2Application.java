510b8dee180ec6722e9d1de11e60d57c0508900b
yukarisusaki <yukarisoprano717@gmail.com>
2022-10-05 19:24:27 +0200

    Initial commit

---
+++ src/main/java/bic/gr2/slm_lv2/SlmLv2Application.java
@@ -0,0 +1,13 @@
+package bic.gr2.slm_lv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmLv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmLv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/bic/gr2/slm_lv2/SlmLv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bic.gr2.slm_lv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmLv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
